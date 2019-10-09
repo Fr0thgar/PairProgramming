cc9043c326cbcd9e13bb08fabcb063f6a6f19eb3
Fr0thgar <jona270r@edu.easj.dk>
2019-10-09 09:35:23 +0200

    controller
    controller + static list

---
+++ carsRESTprovider/Controllers/MusicRecordsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using carsRESTprovider.model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace carsRESTprovider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicRecordsController : ControllerBase
+    {
+        public static List<MusicRecord> Records = new List<MusicRecord>()
+        {
+            new MusicRecord("Enough", "Counterfit", "151", "2017", "10"),
+            new MusicRecord("Leibe ist für Alle da", "Rammstein","200", "2015","16")
+        };
+
+        // GET: api/MusicRecords
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/MusicRecords/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/MusicRecords
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/MusicRecords/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
